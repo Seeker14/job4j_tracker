7441394e1790936df0200e612900c29f1bb50985
Seeker14 <allegoriy@list.ru>
2023-03-02 10:58:47 +0200

    0.4. Пользовательские исключения. В цикле добавлен break [#219367 #396400]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -8,6 +8,7 @@ public class FindEl {
         for (int index = 0; index < value.length; index++) {
             if (value[index].equals(key)) {
                 rsl = index;
+                break;
             }
         }
         if (rsl == -1) {
